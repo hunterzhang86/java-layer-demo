d00edc6c41468f9e3e75932e5f9870701f1fa8bb
Hunter <hunterzhang86@gmail.com>
2022-09-17 13:04:42 +0800

    init commit

---
+++ demo-api/src/main/java/com/fflow/demo/api/UserService.java
@@ -0,0 +1,14 @@
+package com.fflow.demo.api;
+
+
+import com.fflow.demo.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
