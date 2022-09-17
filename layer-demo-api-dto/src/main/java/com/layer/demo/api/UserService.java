bd9481e93efee023ebcf56286c03a96bc5d40c2b
Hunter <hunterzhang86@gmail.com>
2022-09-17 22:13:46 +0800

    init commit

---
+++ layer-demo-api-dto/src/main/java/com/layer/demo/api/UserService.java
@@ -0,0 +1,14 @@
+package com.layer.demo.api;
+
+
+import com.layer.demo.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
